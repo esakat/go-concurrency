3460e57a48c2d4dd31b717a879ff54779b622eaa
esakat <esaka.tom@gmail.com>
2022-01-04 13:08:08 +0900

    orDone

--- chap04/4-8.go
+++ chap04/4-8.go
@@ -1,1 +1,27 @@
 package chap04
+
+func orDone(done, c <-chan interface{}) <-chan interface{} {
+	valStream := make(chan interface{})
+	go func() {
+		defer close(valStream)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-c:
+				if ok == false {
+					return
+				}
+				select {
+				case valStream<-v:
+				case <-done:
+				}
+			}
+		}
+	}()
+	return valStream
+}
+
+// Usecase
+// for val := range orDone(done, myChan) { // val に対して何かする　}
+// myChanが外部から取得する場合に、複雑なエラー制御をorDone内に閉じ込めてしまう。
