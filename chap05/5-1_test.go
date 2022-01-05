0213f34e677cf7ba59b62cad2d4b97feaa991eeb
esakat <esaka.tom@gmail.com>
2022-01-05 11:38:32 +0900

    chap5-1

---
+++ chap05/5-1_test.go
@@ -0,0 +1,23 @@
+package chap05
+
+import (
+	"log"
+	"os"
+	"testing"
+)
+
+func Test_runJob(t *testing.T) {
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.Ltime|log.LUTC)
+
+	err := runJob("1")
+	if err != nil {
+		msg := "There was an unexpected issue; please report this as a bug.\n"
+		if _, ok := err.(IntermediateErr); ok {
+			msg = err.Error()
+		}
+		handleError(1, err, msg)
+	}
+
+	log.Println("finish")
+}
