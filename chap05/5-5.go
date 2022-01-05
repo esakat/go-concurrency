120e7ea5ce35619bebd52c05f19d571024cdfe0a
esakat <esaka.tom@gmail.com>
2022-01-05 16:19:24 +0900

    multi limit

--- chap05/5-5.go
+++ chap05/5-5.go
@@ -3,17 +3,25 @@ package chap05
 import (
 	"context"
 	"golang.org/x/time/rate"
+	"sort"
+	"time"
 )
+
+func Per(eventCount int, duration time.Duration) rate.Limit {
+	return rate.Every(duration/time.Duration(eventCount))
+}
 
 // Dummy API with two endpoints
 
 type APIConnection struct {
-	rateLimiter *rate.Limiter
+	rateLimiter RateLimiter
 }
 
 func Open() *APIConnection {
+	secondLimit := rate.NewLimiter(Per(2, time.Second), 1)
+	minuteLimit := rate.NewLimiter(Per(10, time.Minute), 10)
 	return &APIConnection{
-		rateLimiter: rate.NewLimiter(rate.Limit(1), 1),
+		rateLimiter: MultiLimiter(secondLimit, minuteLimit),
 	}
 }
 
@@ -32,3 +40,33 @@ func (a *APIConnection) ResolveAddress(ctx context.Context) error {
 }
 
 
+// 流量制限のまとめやく
+type RateLimiter interface {
+	Wait(ctx context.Context) error
+	Limit() rate.Limit
+}
+
+type multiLimiter struct {
+	limiters []RateLimiter
+}
+
+func MultiLimiter(limiters ...RateLimiter) *multiLimiter {
+	byLimit := func(i, j int) bool {
+		return limiters[i].Limit() < limiters[j].Limit()
+	}
+	sort.Slice(limiters, byLimit)
+	return &multiLimiter{limiters: limiters}
+}
+
+func (l *multiLimiter) Wait(ctx context.Context) error {
+	for _, l := range l.limiters {
+		if err := l.Wait(ctx); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (l *multiLimiter) Limit() rate.Limit {
+	return l.limiters[0].Limit()
+}
